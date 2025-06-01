fe6406f38a054fcfe612a2c740d11ece68868e48
Martin Straus <martinstraus@gmail.com>
2025-06-01 11:51:34 -0300

    Added script for creating versions.

--- create_version.sh
+++ create_version.sh
@@ -1,28 +1,42 @@
 #!/bin/bash
 # filepath: /home/martinstraus/proyectos/life-simulator/create_version.sh
 
-# Usage: ./create_version.sh <version> "<description>"
+# Usage: ./create_version.sh <version>
 
 set -e
 
-if [ $# -lt 2 ]; then
-    echo "Usage: $0 <version> \"<description>\""
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <version>"
     exit 1
 fi
 
 VERSION="$1"
-DESCRIPTION="$2"
 CHANGELOG="changelog.md"
 
-# Add description to changelog
-echo -e "## Version $VERSION\n$DESCRIPTION\n" >> "$CHANGELOG"
+# Get the last tag (if any)
+LAST_TAG=$(git describe --tags --abbrev=0 2>/dev/null || echo "")
+
+# Get log entries since last tag
+if [ -z "$LAST_TAG" ]; then
+    LOG=$(git log --pretty=format:"- %s" )
+else
+    LOG=$(git log "$LAST_TAG"..HEAD --pretty=format:"- %s")
+fi
+
+if [ -z "$LOG" ]; then
+    echo "No new commits since last tag. Aborting."
+    exit 1
+fi
+
+# Add log to changelog
+echo -e "## Version $VERSION\n$LOG\n" >> "$CHANGELOG"
 
 # Add and commit changelog
 git add "$CHANGELOG"
 git commit -m "Update changelog for version $VERSION"
 
-# Create annotated tag
-git tag -a "$VERSION" -m "$DESCRIPTION"
+# Create annotated tag with the log as the message
+git tag -a "$VERSION" -m "Changelog:\n$LOG"
 
 # Push commit and tag
 git push
